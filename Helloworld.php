ec3435aa8ed77f7b4f8ae0a88713f91e6275d0d3
Eric Curley <eric@Erkcurley.net>
2016-08-08 22:24:05 -0400

    Centered the display

--- Helloworld.php
+++ Helloworld.php
@@ -1,15 +1,42 @@
 #!/usr/bin/php
 <?php
-passthru('clear');
-while(true){
-	for($j=0;$j<20;$j++){
-		for($i=1;$i<128;$i++){
-			$num = rand(0,256);
-			echo "\e[48;5;{$num}m \e[0m";
-			usleep(100000);
-		}
-		echo "\n";
-	}
+
+function center(){
+
+	$COLUMNS = exec('tput cols');
+	$LINES = exec('tput lines');
+
+	$width = $COLUMNS - 10;
+	$height = $LINES - 10;
+
 	passthru('clear');
+
+	echo "\033[5;5H";
+
+	block($width,$height);
+
+
 }
-?>
+
+function row($width){
+
+	for($i=1;$i<$width;$i++){
+        	$num = rand(0,256);
+        	echo "\e[48;5;{$num}m \e[0m";
+        	usleep(50000);
+        }
+
+}
+
+function block($width,$height){
+
+	for($j=1;$j<$height;$j++){
+                row($width);
+		$indent = 5 + $j;
+               	echo "\033[{$indent};5H";
+        }
+
+}
+while(true){
+	center();
+}
