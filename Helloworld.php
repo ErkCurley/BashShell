4719792d202677b636a1e3c3c1466659fdfb423c
Eric Curley <eric@Erkcurley.net>
2016-08-20 23:22:33 -0400

    Make it rain on them hoes

--- Helloworld.php
+++ Helloworld.php
@@ -1,7 +1,7 @@
 #!/usr/bin/php
 <?php
 
-function center($args){
+function center($args,$additional = false){
 
 	$COLUMNS = exec('tput cols');
 	$LINES = exec('tput lines');
@@ -13,14 +13,14 @@ function center($args){
 
 	echo "\033[5;5H";
 
-	block($width,$height,$args);
+	block($width,$height,$args,$additional);
 
 
 }
 
-function row($width,$args){
-
-
+function row($width,$args,$additional = false){
+	
+	$line = '';
 	for($i=1;$i<$width;$i++){
 
 		if($args=='blue'){
@@ -31,36 +31,51 @@ function row($width,$args){
                 	$num = rand(46,51);
         	}elseif($args == 'purple'){
                 	$num = rand(88,93);
-        	}else{
-                	$num = rand(1,256);
-        	}        	
+ 		}    	
 
-        	echo "\e[48;5;{$num}m \e[0m";
-        	usleep(50000);
+		if($additional == true){
+			$num = rand(16,21);
+			$chance = rand(1,100);
+			if($chance < 95){
+        			$line .= "\e[48;5;16m \e[0m";
+        		}else{
+				$line .= "\e[48;5;{$num}m \e[0m";
+			}
+		}
+
         }
+	
+	echo $line;
+	usleep(2000000);
 
 }
 
-function block($width,$height,$args){
+function block($width,$height,$args,$additional = false){
 
 	for($j=1;$j<$height;$j++){
-                row($width,$args);
+                row($width,$args,$additional);
 		$indent = 5 + $j;
                	echo "\033[{$indent};5H";
         }
 
 }
 while(true){
-	$color = rand(1,5);
-	if($color == 1){
-		center('blue');	
-	}elseif($color == 2){
-		center('red');
-	}elseif($color == 3){
-		center('green');
-	}elseif($color == 4){
-		center('purple');
+	$rain = true;
+	if($rain == true){
+		center('blue','rain');
 	}else{
-		center('');
+
+		$color = rand(1,5);
+		if($color == 1){
+			center('blue');	
+		}elseif($color == 2){
+			center('red');
+		}elseif($color == 3){
+			center('green');
+		}elseif($color == 4){
+			center('purple');
+		}else{
+			center('');
+		}
 	}
 }
